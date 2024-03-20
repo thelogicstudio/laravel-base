e687a7a4f25621a945984478c172f1e01b74806f
ed <ed@logicstudio.nz>
2024-03-21 10:54:13 +1300

    Profile page update

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -27,23 +27,23 @@
                 <div class="col-sm-12">
                     <div class="card">
                         <div class="card-body">
-                            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800">
+                            <div class="p-1 sm:p-8 bg-white dark:bg-gray-800">
                                 <div class="max-w-xl">
                                     @include('profile.partials.update-profile-information-form')
                                 </div>
                             </div>
 
-                            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800">
+                            <div class="p-1 sm:p-8 bg-white dark:bg-gray-800 mt-5">
                                 <div class="max-w-xl">
                                     @include('profile.partials.update-password-form')
                                 </div>
                             </div>
 
-                {{--            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">--}}
-                {{--                <div class="max-w-xl">--}}
-                {{--                    @include('profile.partials.delete-user-form')--}}
-                {{--                </div>--}}
-                {{--            </div>--}}
+                            {{--            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">--}}
+                            {{--                <div class="max-w-xl">--}}
+                            {{--                    @include('profile.partials.delete-user-form')--}}
+                            {{--                </div>--}}
+                            {{--            </div>--}}
                         </div>
                     </div>
                 </div>
