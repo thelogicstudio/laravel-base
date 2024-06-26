aaaeb7d5f13c48772e463193f540bead47211f94
ed <ed@logicstudio.nz>
2024-06-26 13:37:41 +1200

    User seeder updated

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,10 +8,10 @@
 
         public function run() {
             \App\Models\User::create([
-                'name' => 'The Logic Studio',
-                'email' => 'notify@logicstudio.nz',
+                'name' => 'ABC Company',
+                'email' => 'admin@abc.com',
                 'email_verified_at' => now(),
-                'password' => Hash::make('fred21')
+                'password' => Hash::make('admin123')
             ]);
         }
 	}
